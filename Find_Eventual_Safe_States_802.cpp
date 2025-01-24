56bdbd9ae8b1f7c2bb210679f22428a5754f5e7a
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-01-24 18:54:18 +0530

    Add files via upload

---
+++ Find_Eventual_Safe_States_802.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit,
+             vector<bool>& inStack) {
+        if (inStack[node]) {
+            return true;
+        }
+        if (visit[node]) {
+            return false;
+        }
+        visit[node] = true;
+        inStack[node] = true;
+        for (auto neighbor : adj[node]) {
+            if (dfs(neighbor, adj, visit, inStack)) {
+                return true;
+            }
+        }
+        inStack[node] = false;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<bool> visit(n), inStack(n);
+        for (int i = 0; i < n; i++) {
+            dfs(i, graph, visit, inStack);
+        }
+        vector<int> safeNodes;
+        for (int i = 0; i < n; i++) {
+            if (!inStack[i]) {
+                safeNodes.push_back(i);
+            }
+        }
+        return safeNodes;
+    }
+
+int main(){
+
+    vector<vector<int>> graph = {{1,2},{2,3},{5},{0},{5},{},{}};
+    vector<int> result = eventualSafeNodes(graph);
+    for(auto x: result){
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
